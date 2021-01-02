19ffabae8042050c5cb539b6a4b9a883622ab220
yeshwanth konka <Yeshwanth Konka>
2021-01-02 09:18:58 +0530

    ticket_managment_initial_check

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/about">
+        <h1>About</h1>
+      </Route>
+      <Route path="/line">
+        <h1>Line-2</h1>
+      </Route>
+    </Router>
   );
 }
 
